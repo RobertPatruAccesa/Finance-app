aff0c65eceebe603b71021b2d627d82537a8cf05
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2022-12-23 15:20:27 +0200

    wre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Finance-app';
+	title = 'Finance-app';
+
+	constructor(private http: HttpClient) {
+		http.get('/api').subscribe(data => console.log(data))
+	}
 }
