272fdf01dc8eddf044b37645042ec9e3973008b8
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-10 13:45:36 +0200

    a

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent {
-
+	transactions: any = [
+		{
+			userId: "1",
+			transactionId: "1",
+			amount: "100.00",
+			type: "payment",
+			date: "23.12.2022",
+			description: "Electricity bill"
+		},
+		{
+			userId: "1",
+			transactionId: "2",
+			amount: "1000.00",
+			type: "payment",
+			date: "02.01.2023",
+			description: "Paying Rent"
+		},
+		{
+			userId: "1",
+			transactionId: "3",
+			amount: "100.00",
+			type: "payment",
+			date: "23.12.2022",
+			description: "Electricity bill"
+		}
+	]
 }
