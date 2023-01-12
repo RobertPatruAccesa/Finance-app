4b83c67dfc4ad3d3ee2072bbaf1662830e04a936
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-12 12:48:46 +0200

    Transaction details works (visually)

--- src/app/create-transaction/create-transaction.module.ts
+++ src/app/create-transaction/create-transaction.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateTransactionComponent } from './create-transaction.component';
 import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../shared/button/button.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [CreateTransactionComponent],
-	imports: [CommonModule, FormsModule],
+	imports: [CommonModule, FormsModule, SharedModule]
 })
 export class CreateTransactionModule {}
