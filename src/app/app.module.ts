758f5d03836a26cd898e5c1046e4670ffe8b990a
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-12 14:47:23 +0200

    app-button error is solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { CreateTransactionModule } from './create-transaction/create-transaction.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -7,11 +8,12 @@ import { HomeModule } from './home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @NgModule({
 	declarations: [AppComponent],
 	providers: [],
 	bootstrap: [AppComponent],
-	imports: [BrowserModule, HttpClientModule, SharedModule, HomeModule, RouterModule, FormsModule]
+	imports: [BrowserModule, HttpClientModule, SharedModule, HomeModule, CreateTransactionModule, TransactionsModule, RouterModule, FormsModule]
 })
 export class AppModule {}
