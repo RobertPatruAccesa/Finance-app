80368b1fbcaaf1c3ef603fb647e312d3c603b9a3
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-17 17:51:44 +0200

    First reducer doen't work

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,18 @@
-import { ActionReducer ,ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from "@ngrx/store";
+import { ActionReducerMap, createReducer, on, State } from '@ngrx/store';
+import { Transaction } from '../core/interfaces/transaction.interface';
+import { TransactionActions } from '../transactions/action-types';
 
 export interface TransactionsState {
-
+	transactions: Transaction[];
 }
 
-export const reducers: ActionReducerMap<TransactionsState> = {
-	
-}
+export const initialTransactionState: Transaction[] = [];
+
+export const transactionsReducer = createReducer(
+	initialTransactionState,
+	on(TransactionActions.getAllTransactions, (state, action) => {
+		return {
+			transactions: action.transactions
+		}
+	})
+)
