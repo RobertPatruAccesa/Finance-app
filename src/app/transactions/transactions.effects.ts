e133f112088237f3ee305e2f84f965498c39a7fd
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-18 17:23:41 +0200

    Simple Side Effect created

--- src/app/transactions/transactions.effects.ts
+++ src/app/transactions/transactions.effects.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Actions } from '@ngrx/effects';
 
 @Injectable()
 
 export class TransactionsEffect {
-	constructor () {
-		
+	constructor (private actions$: Actions) {
+		actions$.subscribe(action => {
+			if(action.type == '[Transactions Page] Get All Transactions') {
+				console.log('Side Effect: ', 'Transactions actions activated')
+			}
+		})
 	}
 }
