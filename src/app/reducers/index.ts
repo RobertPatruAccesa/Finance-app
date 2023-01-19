7fc65346120665984ff7a418affdabbaa3c525b2
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-01-19 14:55:59 +0200

    All compilation errors are soled. But can't get transactions

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,20 +1,22 @@
-import { createReducer, on } from '@ngrx/store';
+import { ActionReducerMap, createReducer, on, State } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+
 import { Transaction } from '../core/interfaces/transaction.interface';
 import { TransactionActions } from '../transactions/action-types';
 
-export interface TransactionsState {
-	transactions: Transaction[];
+export interface TransactionsState extends EntityState<Transaction> {
+	enteties: { [key: string]: Transaction };
+	ids: string[];
 }
 
-export const initialTransactionState: TransactionsState = {
-	transactions : []
-};
+export const adapter = createEntityAdapter<Transaction>();
+
+export const initialTransactionsState = adapter.getInitialState();
 
 export const transactionsReducer = createReducer(
-	initialTransactionState,
-	on(TransactionActions.loadAllTransactions, (state, action) => {
-		return {
-			transactions: action.transactions
-		}
-	})
-)
+	initialTransactionsState,
+
+	// instead of "addMany" in the course is used "addAll"
+	// in my case, the compiler says that there is no "addAll" so I used "addMany"
+	on(TransactionActions.allTransactionsLoaded, (state, action) => adapter.addMany(action.transactions, state))
+);
