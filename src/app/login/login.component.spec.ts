05aaa52daa936ab0cb1bba1ceab59a2dc9b71268
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-03-01 14:05:31 +0200

    b

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { Store } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
+import { usersReducer } from '../store/user/user.reducer';
+import { ButtonComponent } from '../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+fdescribe('LoginComponent', () => {
+	let component: LoginComponent;
+	let fixture: ComponentFixture<LoginComponent>;
+    let el: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [LoginComponent, ButtonComponent],
+			providers: [Store],
+			imports: [StoreModule.forRoot({ usersReducer }), FormsModule]
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(LoginComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+        el = fixture.debugElement;
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+    it('should have username and password inputs', () => {
+        const usernameInput = el.query(By.css('.username'));
+        expect(usernameInput).toBeTruthy("Could find username input");
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
