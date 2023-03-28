b08d825c7d3ff49d2108fd506abb694c7bbd6ce5
RobertPatruAccesa <113914427+RobertPatruAccesa@users.noreply.github.com>
2023-03-28 11:24:53 +0300

    initializing firebase, adding a mock data

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -3,7 +3,11 @@ import { Transaction } from '../core/interfaces/transaction.interface';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { deployGetAllTransactionsAction } from '../store/transactions/transactions.actions';
-import { getTransactionsSelector } from '../store/transactions/transactions.selector'; 
+import { getTransactionsSelector } from '../store/transactions/transactions.selector';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
 
 @Component({
 	selector: 'app-transactions',
@@ -13,10 +17,33 @@ import { getTransactionsSelector } from '../store/transactions/transactions.sele
 export class TransactionsComponent implements OnInit {
 	transactions$!: Observable<Transaction[]>;
 
+	firebaseConfig = {
+		apiKey: 'AIzaSyDRvRNjS5dmFFvbGDU27rEm2rMAhCZUMN8',
+		authDomain: 'finance-app-8224e.firebaseapp.com',
+		projectId: 'finance-app-8224e',
+		storageBucket: 'finance-app-8224e.appspot.com',
+		messagingSenderId: '876942998162',
+		appId: '1:876942998162:web:aedc332f871602a076c44b',
+		measurementId: 'G-RZ7VHPE692'
+	};
+	app = initializeApp(this.firebaseConfig);
+	db = getFirestore(this.app);
+
 	constructor(private store: Store<{ transactions: Transaction[] }>) {}
 
-	ngOnInit(): void {
-		this.store.dispatch(deployGetAllTransactionsAction())
-		this.transactions$ = this.store.pipe(select(getTransactionsSelector))
+	async ngOnInit(): Promise<void> {
+		this.store.dispatch(deployGetAllTransactionsAction());
+		this.transactions$ = this.store.pipe(select(getTransactionsSelector));
+
+		try {
+			const docRef = await addDoc(collection(this.db, 'users'), {
+				first: 'Ada',
+				last: 'Lovelace',
+				born: 1815
+			});
+			console.log('Document written with ID: ', docRef.id);
+		} catch (e) {
+			console.error('Error adding document: ', e);
+		}
 	}
 }
